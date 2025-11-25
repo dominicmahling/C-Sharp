3436cf4eaceca78a2704f5c98a56d0eee908f544
d0s1 <dominic1020@web.de>
2025-11-25 03:06:19 +0100

    some code for starting

---
+++ main/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+var userid = 0;
+var Bog = new Bogus.Faker<User>()
+    .RuleFor( u => u.Id, f => userid++)
+    .RuleFor(u => u.Name, f => Guid.NewGuid().ToString());
+
+List<User> users = Bog.Generate(20);
+foreach (User user in users)
+{
+    Console.WriteLine($"{user.Id}\t{user.Name}");
+}
+
+class User()
+{
+    public int Id{get;set;}
+    public string? Name{get;set;}
+
+    public User(string name):this()
+    {
+        Name = name;
+    }    
+}
